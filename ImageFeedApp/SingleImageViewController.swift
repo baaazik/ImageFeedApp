e75182092294d66ee03be7181730bde39260435d
Anzhelika Zabaznova <zabaznova2303@gmail.com>
2025-05-27 16:59:51 +0300

    feature: add tab bar controller and single image controller

---
+++ ImageFeedApp/SingleImageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeedApp
+//
+//  Created by Анжелика Забазнова on 27.05.2025.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
