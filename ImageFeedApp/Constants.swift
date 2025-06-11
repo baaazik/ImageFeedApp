e58da3bbd3335ad12bfddf639f144fb35756d72f
Anzhelika Zabaznova <zabaznova2303@gmail.com>
2025-06-11 03:05:12 +0300

    feature: add API constants

---
+++ ImageFeedApp/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeedApp
+//
+//  Created by Анжелика Забазнова on 09.06.2025.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "h7X7c_w4d5LtLtaIMNXyaFnOMQH8u2cxPyC_tOB2Q8c"
+    static let secretKey = "u0N2iyWMteTDx04z8H_a6BGBSDG1MH8OPpl_0Gp3pRM"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL: URL = URL(string: "https://api.unsplash.com/")!
+}
